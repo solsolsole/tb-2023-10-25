43e980c83e518156ef458940e274030e1e3db9fb
solsolsole <dmsthf917@naver.com>
2023-10-26 10:40:25 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,17 +7,27 @@ class App {
         System.out.println("==명언앱==");
 
         while (true) {
+
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in); //system out 모니터 in은 키보드 즉 이 말은 사용자의 키보드를 스캔한다.
             String cmd = scanner.nextLine(); //넥스트라인 == 정지
 
-            if ( cmd.equals("종료")) { //String의 경우에는 == 를 사용하지 않고 .equals("dd") 를 사용한다!
-                break;
+            if (cmd.equals("등록")){
+                System.out.print("명언 : ");
+                scanner.nextLine();
+                System.out.print("작가 : ");
+                scanner.nextLine();
             }
 
-            System.out.printf("입력받은 명령어 : %s\n", cmd); // %s가 cmd를 치환한다.
-                                //== ("입력받은 명령어 : "+ cmd + "\n");
+                if (cmd.equals("종료")) { //String의 경우에는 == 를 사용하지 않고 .equals("dd") 를 사용한다!
+                    break;
+                }
+
+                //printf 이건 문장안에 빈칸[%s(문자) %d(정수)] 쓸 수 있음.
+
+
+
         }
     }
 }
